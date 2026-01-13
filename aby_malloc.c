fa4c61748e379f26cabd32ee74e92a9dc76e53d6
aby-20 <abysyril1919@gmail.com>
2026-01-13 08:58:22 +0000

    Add initial custom malloc implementation

---
+++ aby_malloc.c
@@ -0,0 +1,44 @@
+#define ALIGN 16
+#define ALLOCATED 1UL
+#define ALIGN_UP(x) (((x)+ALIGN-1) & ~(ALIGN-1))
+#include <unistd.h>
+#include <stddef.h>
+#include <stdio.h>
+
+typedef struct block_header{
+size_t size_and_flag;
+}block_header_t;
+
+void* aby_malloc(size_t size){
+if(size == 0)
+return NULL;
+
+size_t total_size = ALIGN_UP(sizeof(block_header_t)+size);
+
+block_header_t *block = sbrk(total_size);
+
+if(block == (void*)-1)
+return NULL;
+
+block -> size_and_flag = total_size | ALLOCATED;
+
+return (void *)(block +1);
+}
+
+int main(void){
+int *p = aby_malloc(sizeof(int)*10);
+
+if(!p){
+printf("allocation failed\n");
+return 1;
+}
+
+p[0] = 41;
+p[9] = 99;
+
+printf("p[0] = %d\n",p[0]);
+printf("p[9] = %d\n",p[9]);
+
+return 0;
+}
+
